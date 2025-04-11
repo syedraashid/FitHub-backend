4ceb113faa70510f4ea62edc2d19fc0d4640a9bc
syedraashid <raashidsyed955@gmail.com>
2025-04-11 11:23:43 +0530

    addded the OTEL Configuration

--- FitHub.Endpoints/Program.cs
+++ FitHub.Endpoints/Program.cs
@@ -1,5 +1,10 @@
 using FitHub.Endpoints.ServiceConfigurations;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using System.IdentityModel.Tokens.Jwt;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Instrumentation.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +14,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
+    {
+        tracerProviderBuilder
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("FitnessApi") // Your service name
+            .AddAttributes(new Dictionary<string, object>
+                {
+                    { "deployment.environment", builder.Environment.EnvironmentName }
+                }))
+            .AddAspNetCoreInstrumentation() // Captures incoming API requests
+            .AddHttpClientInstrumentation() // Captures outgoing HTTP requests
+            .AddSqlClientInstrumentation()
+            .AddEntityFrameworkCoreInstrumentation(options =>{
+                options.SetDbStatementForText = true;
+            })// Captures SQL Server queries
+            .AddJaegerExporter(opts =>
+            {
+                opts.AgentHost = "jaeger"; // Change if Jaeger is hosted elsewhere
+                opts.AgentPort = 6831;
+            });
+    });
 
 var app = builder.Build();
 
